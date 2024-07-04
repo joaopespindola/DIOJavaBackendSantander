112b801bf3826a2cbdd69951db815a2c629b4b3b
joaopespindola <joaopedro.espindola@outlook.com>
2024-07-03 21:26:09 -0300

    Adicionando o Desafio9

--- StreamAPI/src/main/java/org/desafios/Desafio8.java
+++ StreamAPI/src/main/java/org/desafios/Desafio8.java
@@ -7,10 +7,10 @@ public class Desafio8 {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
         Optional<Integer> somaLista = numeros.stream()
-                        .reduce((a,b) -> Integer.sum(a,b));// Metodo para agregar os elementos de um stream em um unico resultado
+                .reduce((a, b) -> Integer.sum(a, b));// Metodo para agregar os elementos de um stream em um unico resultado
 
-        if(somaLista.isPresent()) {
+        if (somaLista.isPresent()) {
             System.out.println("A soma eh " + somaLista.get());
-        }else System.out.println("A lista esta vazia");
+        } else System.out.println("A lista esta vazia");
     }
 }
---
+++ StreamAPI/src/main/java/org/desafios/Desafio9.java
@@ -0,0 +1,19 @@
+package org.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio9 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);  
+
+        boolean saoDistintos = numeros.stream().distinct().count() == numeros.size();// Verifica se o numero de elementos
+                                                                                    // distintos eh o mesmo numero de elementos na lista numeros
+
+        if (saoDistintos) {
+            System.out.println("Sao distintos");
+        }else{
+            System.out.println("Nao distintos");
+        }
+    }
+}
