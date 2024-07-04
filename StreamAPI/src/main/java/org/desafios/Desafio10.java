c55cd54677fe0c523fbcff09fbf7ec6f8dcf20bc
joaopespindola <joaopedro.espindola@outlook.com>
2024-07-03 21:35:10 -0300

    Adicionando o Desafio10

---
+++ StreamAPI/src/main/java/org/desafios/Desafio10.java
@@ -0,0 +1,17 @@
+package org.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio10 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> multiplosTresOuCinco = numeros.stream()
+                .filter(n -> n % 3 == 0 || n % 5 == 0)
+                .distinct()
+                .toList();
+
+        System.out.println(multiplosTresOuCinco);
+    }
+}
--- StreamAPI/src/main/java/org/desafios/Desafio9.java
+++ StreamAPI/src/main/java/org/desafios/Desafio9.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Desafio9 {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);  
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
         boolean saoDistintos = numeros.stream().distinct().count() == numeros.size();// Verifica se o numero de elementos
                                                                                     // distintos eh o mesmo numero de elementos na lista numeros
