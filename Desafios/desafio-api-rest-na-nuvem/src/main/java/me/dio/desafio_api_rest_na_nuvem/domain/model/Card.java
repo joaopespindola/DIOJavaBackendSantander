d72b4a7139e6075fcf0262cbe44a2805958fdda7
joaopespindola <joaopedro.espindola@outlook.com>
2024-07-28 23:26:28 -0300

    feat: Adicionando o package model

---
+++ Desafios/desafio-api-rest-na-nuvem/src/main/java/me/dio/desafio_api_rest_na_nuvem/domain/model/Card.java
@@ -0,0 +1,25 @@
+package me.dio.desafio_api_rest_na_nuvem.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+
+@Entity(name = "tb_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(name = "available_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
+
+}
---
+++ Desafios/desafio-api-rest-na-nuvem/src/main/java/me/dio/desafio_api_rest_na_nuvem/domain/model/News.java
@@ -0,0 +1,7 @@
+package me.dio.desafio_api_rest_na_nuvem.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+}
