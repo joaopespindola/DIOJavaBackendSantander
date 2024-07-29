fb76af5cd2ed8213fe79899cda1a3932796b1e44
joaopespindola <joaopedro.espindola@outlook.com>
2024-07-29 19:08:44 -0300

    chore: config apllication-dev.yml

---
+++ Desafios/desafio-api-rest-na-nuvem/src/main/java/me/dio/desafio_api_rest_na_nuvem/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package me.dio.desafio_api_rest_na_nuvem.domain.repository;
+
+import me.dio.desafio_api_rest_na_nuvem.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
