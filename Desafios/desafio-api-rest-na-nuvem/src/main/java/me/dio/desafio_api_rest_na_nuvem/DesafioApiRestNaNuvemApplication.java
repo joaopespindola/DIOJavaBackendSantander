16f45487cefa6973a87d35b3a1670df1da6e6482
joaopespindola <joaopedro.espindola@outlook.com>
2024-07-28 22:18:33 -0300

    Setup inicial do projeto

---
+++ Desafios/desafio-api-rest-na-nuvem/src/main/java/me/dio/desafio_api_rest_na_nuvem/DesafioApiRestNaNuvemApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_api_rest_na_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiRestNaNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiRestNaNuvemApplication.class, args);
+	}
+
+}
---
+++ Desafios/desafio-api-rest-na-nuvem/src/test/java/me/dio/desafio_api_rest_na_nuvem/DesafioApiRestNaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_api_rest_na_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiRestNaNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
